733b13c03ded22eef67baf3e8d3f56faa2716c89
Petru <otgon.petru@gmail.com>
2023-05-16 19:42:59 +0200

    continued fifth advent

--- fifthAdvent/fifthAdvent.js
+++ fifthAdvent/fifthAdvent.js
@@ -1,33 +1,36 @@
 const fs = require('fs');
-fs.readFile("adventArray.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    const lines = data.split("\n");
-})
-
-fs.readFile("adventMoves.txt", "utf8", (err, data) => {
-    if (err) throw err;
-    const lines = data.split("\n");
-    lines.forEach((line) => {
-        line = line.split(" ");
-        console.log(line);
+fs.readFile("adventArray.txt", "utf8", (err, arrayInput) => {
+    if (err) throw err
+    const arrayData = arrayMaker(arrayInput.split("\n"))
+    fs.readFile("adventMoves.txt", "utf8", (err, moveInput) => {
+        if (err) throw err
+        const moveData = moveReader(moveInput.split("\n"))
+        console.log(moveData)
     })
 })
 
 function arrayMaker(input) {
-    const localArr = [];
-     for (let k = 1; k <= 33; k += 4) {
-         const localSubArr = [];
-         for (let i = 7; i >= 0; i--) {
+    const localArr = []
+    for (let k = 1; k <= 33; k += 4) {
+        const localSubArr = []
+        for (let i = 7; i >= 0; i--) {
             if (input[i][k] && input[i][k] !== " ") {
-                localSubArr.push(input[i][k]);
+                localSubArr.push(input[i][k])
             }
         }
-        localArr.push(localSubArr);
+        localArr.push(localSubArr)
     }
-    return localArr;
+    return localArr
 }
 
-/*function moveReader(input){
-    let trimmedInput = input.split(" ");
-    return trimmedInput
-}*/
+function moveReader(input) {
+    const mappedInput = input.map((line) => {  return line.split(" ") })
+    return mappedInput.map((line) => {
+        return line.splice(0,4)
+    })
+}
+
+
+function moveMaker(input) {
+
+}
