8dec5fbcea3127e1bc3ea8b8c6a00a93ced17ad8
Petru <otgon.petru@gmail.com>
2023-05-16 11:50:14 +0200

    finished fourthAdvent.js

--- fourthAdvent/fourthAdvent.js
+++ fourthAdvent/fourthAdvent.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
 fs.readFile("advent.txt", "utf8", (err, data) => {
     if (err) throw err;
-    let lines = data.split(",");
-    const newLines = lines.map((str) => {
-        return str.replace(/\r?\n|\r/g, ',');
-    });
-    console.log(newLines);
-})
+    let totalPairs = 0;
+    let lines = data.split("\n");
+    lines = lines.map(lines => lines.split(","));
+    for (const pairs of lines) {
+        let pair1Start = +pairs[0].split("-")[0];
+        let pair1End = +pairs[0].split("-")[1];
+        let pair2Start = +pairs[1].split("-")[0];
+        let pair2End = +pairs[1].split("-")[1];
+        if (pair1Start < pair2Start && pair1End < pair2Start){
+            console.log("No overlap");
+        } else if (pair2Start < pair1Start && pair2End < pair1Start){
+            console.log("No overlap");
+        } else {
+            totalPairs++;
+        }
+    }
+    console.log(totalPairs);
+});
